b76fb3266a07e59d77165010ec7e7639751413b9
soumic sarkar <soumic@codillion.com>
2025-09-15 09:05:02 +0530

    Refactor HomeC and Home components to improve layout; adjust main container width and add padding for better responsiveness.

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,6 +1,5 @@
 import AppLayout from "../shared/AppLayout";
 import { useLang } from "../modules/lang/LangContext";
-import HomeDashboard from "../components/homepage/Chart";
 import HindiBooksSalesDashboard from "../components/homepage/HomeC";
 // import HindiBooksSalesDashboard from "../components/homepage/HomeC";
 
@@ -8,8 +7,11 @@ export default function Home() {
   const { t } = useLang();
   return (
     <AppLayout>
+      <div className="mx-auto w-full py-6">
+
       <h1 className="text-3xl font-bold text-gray-900">{t("welcome")}</h1>
       <p className=" text-gray-600">{t("welcome_subtitle")}</p>
+      </div>
       <div>
         <HindiBooksSalesDashboard />
       </div>
